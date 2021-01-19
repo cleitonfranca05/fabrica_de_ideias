2a27a1f32278f7325cf2fa6581f7f9088f8a70ae
CLeiToN FRaNÇa <cleitonfranca05@gmail.com>
2021-01-18 23:10:42 -0300

    Desafio Flutterando Home do Tinder

--- Quem_Tem_Doa/quem_tem_doa/lib/desafio.dart
+++ Quem_Tem_Doa/quem_tem_doa/lib/desafio.dart
@@ -13,9 +13,52 @@ class _DesafioPageState extends State<DesafioPage> {
         backgroundColor: Colors.red[400],
         title: Text('Desafio Flutterando'),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Container(height: 20),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 250,
+                    child: Image.asset('assets/images/tinder.png'),
+                  ),
+                  Text("Location Changer",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.red[600])),
+                  Container(height: 10),
+                  Text("Plugin app for Tinder",
+                      style: TextStyle(
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.red[600])),
+                  Container(height: 50),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60.0),
+                    ),
+                    textColor: Colors.white,
+                    color: Colors.red[300],
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 20, left: 50, right: 50, bottom: 20),
+                      child: Text('Login with Facebook',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
